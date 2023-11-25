067b5fe5083086a26cd7181aa672080a62644923
CleiverMejia <CleiverMejiaR@gmail.com>
2023-11-25 11:58:07 -0500

    test

--- src/Taylor/Math/Algebra.java
+++ src/Taylor/Math/Algebra.java
@@ -9,84 +9,22 @@ package Taylor.Math;
 import java.math.BigDecimal;
  
 public class Algebra{
+    public static double Factorial(double n) {
+        return (n < 0) ? Double.NaN :
+			   (n <= 1) ? 1 : n * Factorial(n-1);
+    }
 
-	public static double Factorial(double n){
+	public static BigDecimal bigFactorial(BigDecimal n) {
+        return (n.compareTo(BigDecimal.ZERO) < 0) ? BigDecimal.ZERO :
+               ((n.compareTo(BigDecimal.ONE) < 1) ? BigDecimal.ONE :
+               n.multiply(bigFactorial(n.subtract(BigDecimal.ONE))));
+    }
 
-		if (n>0 && n%1==0){
-
-			return bigFactorial( (long) n).doubleValue();
-			
-		}else if (n%1!=0 && n!=0){
-			
-			return 0;
-			
-			//producto = GammaEuler(n);
-			
-		}else if (n==0){
-
-			return 1;
-			
-		}else{
-			
-			return Double.NaN;
-			
-		}
-		
-	}
-	
-	private static long indiceFactorial = -3;
-	private static BigDecimal factorial = new BigDecimal(0);
-	
-	public static BigDecimal bigFactorial(long n){
-		
-		if (n==0 || n==1){
-			
-			return new BigDecimal(1);
-			
-		}
-		
-		BigDecimal fact = new BigDecimal(n);
-		
-		if (indiceFactorial==n-1){
-			
-			factorial = factorial.multiply(new BigDecimal(n));
-			indiceFactorial = n;
-			
-			return factorial;
-			
-		}else if (indiceFactorial==n-2){
-			
-			factorial = factorial.multiply(new BigDecimal(n-1));
-			factorial = factorial.multiply(new BigDecimal(n));
-			indiceFactorial = n;
-			
-			return factorial;
-			
-		}else{
-			
-			factorial = fact.multiply(bigFactorial(n-1));
-			indiceFactorial = n;
-			
-			return factorial;
-			
-		}
-		
-	}
-	
-	public static BigDecimal bigFactorial(BigDecimal n){
-		
-		if (n.compareTo(new BigDecimal(0))==0 || n.compareTo(new BigDecimal(1))==1){
-			
-			return new BigDecimal(1);
-			
-		}
-		
-		BigDecimal fact = n;
-		
-		fact = fact.multiply(bigFactorial(n.subtract(new BigDecimal(-1))));
-		
-		return fact;
-		
-	}
-
+    public static BigDecimal bigFactorial(long n) {
+        BigDecimal number = new BigDecimal(n);
+        
+        return (number.compareTo(BigDecimal.ZERO) < 0) ? BigDecimal.ZERO :
+               ((number.compareTo(BigDecimal.ONE) < 1) ? BigDecimal.ONE :
+               number.multiply(bigFactorial(number.longValue()-1)));
+    }
 }
